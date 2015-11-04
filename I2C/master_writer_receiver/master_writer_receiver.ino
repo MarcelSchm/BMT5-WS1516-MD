10ddc5a1589d37743e6bb3f9697f1fc572f5a60f
Marcel <marcel.schmid@haw-hamburg.de>
2015-11-04 16:16:47 +0100

    debug mode und bessere variablen bezeichnungen

--- I2C/master_writer_receiver/master_writer_receiver.ino
+++ I2C/master_writer_receiver/master_writer_receiver.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include "master_to_slave.h"
 
+// if no serial.print wanted, comment DEBUG in "master_to_slave.h" out
+
 masterToSlave *Kommunikation;
 
 
@@ -9,12 +11,12 @@ Kommunikation = new masterToSlave;
 }
 
 void loop(){
-Kommunikation->sendXY(42,47);
-Kommunikation->sendXY(5,64);
+Kommunikation->sendXY(42,47);// example position
+Kommunikation->sendXY(5,64);// example position
   delay(500);
   
-  Kommunikation->receiveXY();
-  Serial.println(Kommunikation->getX());
+  Kommunikation->receiveXY();//get position from other vehicle. You can see the position as serial print...
+  Serial.println(Kommunikation->getX());// or use this member function to get position
   delay(100);
 }
 
--- I2C/slave_receiver/slave_receiver.ino
+++ I2C/slave_receiver/slave_receiver.ino
@@ -1,11 +1,13 @@
 #include <Wire.h>
 #include <arduino.h>
 
+#define DEBUG // comment out for non Debug Run(no Serial.prints)
+
 byte data[5];
-int posX1=64;//transmit
-int posY1=7;//transmit
-int posX2=1;//receive
-int posY2=2;//receive
+int ourX=64;//transmit
+int ourY=7;//transmit
+int enemyX=1;//receive
+int enemyY=2;//receive
 
 
 void setup(){
@@ -13,7 +15,9 @@ void setup(){
   Wire.begin(8);                // join i2c bus with address #8(ID3.10)
   Wire.onReceive(receiveEvent); // register receiving message from master
    Wire.onRequest(requestEvent); // register request from master
+   #ifdef DEBUG
   Serial.begin(9600);           // start serial for output
+  #endif
 }
 
 void loop(){
@@ -27,34 +31,45 @@ void receiveEvent(int howMany){
   if (5 == howMany){  //are there as many bytes as expected
   while(4 < Wire.available()){  // loop through all but the XY position
     char c = Wire.read(); // receive byte as a character
+    #ifdef DEBUG
     Serial.print(c);       
+    #endif
     if( '@' == c) {  //if startframe is "@"
     int i=1;// data[0] reserved for startframe
   data[i++]= Wire.read();    // receive X1byte as an integer
     data[i++]= Wire.read();    // receive X2byte as an integer
       data[i++]= Wire.read();    // receive Y1byte as an integer
         data[i++]= Wire.read();    // receive Y2byte as an integer
-  posX2=(data[1] << 8) | data[2];
-  posY2=(data[3] << 8) | data[4];
-  Serial.print(posX2);         // print the integer
-  Serial.println(posY2);         // print the integer
+  enemyX=(data[1] << 8) | data[2];
+  enemyY=(data[3] << 8) | data[4];
+  #ifdef DEBUG
+  Serial.print(enemyX);         // print the integer
+  Serial.println(enemyY);         // print the integer
+  #endif
     }
-    else Serial.println("missing startframe");
-  }
+    else 
+    #ifdef DEBUG
+    Serial.println("missing startframe");
+    #endif
+  ;}
     }
-  else Serial.println("Error, Datengroesse anders als in ID3.3 festgelegt");
+  else 
+  #ifdef DEBUG
+  Serial.println("Error, data not as expected (determinated in ID3.3)");
+  #endif
   // BT send Code here? 
-}
+;}
 
 void requestEvent(){ // respond with message of 5 bytes as expected by master
 // BT receive code here?
 int i=0;
 data[i++]= '@';
-data[i++]= highByte(posX1);
-data[i++]= lowByte(posX1);
-data[i++]= highByte(posY1);
-data[i++]= lowByte(posY1);
+data[i++]= highByte(ourX);
+data[i++]= lowByte(ourX);
+data[i++]= highByte(ourY);
+data[i++]= lowByte(ourY);
   Wire.write(data, 5);//necessary to write everything as data stream
-    Serial.print("Transmitted");
-  
+  #ifdef DEBUG
+    Serial.println("Transmitted");
+    #endif 
 }

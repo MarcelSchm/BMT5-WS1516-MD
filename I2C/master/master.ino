fe51e2b4e413fec5eb6b93cc9ae0115798ea4301
Marcel <marcel.schmid@haw-hamburg.de>
2015-11-05 22:19:51 +0100

    bessere programmnamen

--- I2C/master/master.ino
+++ I2C/master/master.ino
@@ -1,28 +1,22 @@
 #include <Wire.h>
-#include "I2C.h"
+#include "master_to_slave.h"
 
-#define XPos 120
-#define YPos 45
+// if no serial.print wanted, comment DEBUG in "master_to_slave.h" out
 
-I2C myConnection(8); 
+masterToSlave *Kommunikation;
 
-void setup() {
-  // put your setup code here, to run once:
-  Wire.begin();
-Serial.begin(9600);
+
+void setup(){
+Kommunikation = new masterToSlave;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
- myConnection.receiveXY();
- myConnection.sendXY(24,42);
- 
- 
- 
-//  Wire.beginTransmission(44);                             
-//  Wire.write('@');            
-//  Wire.write(XPos);
-//  Wire.write(YPos);  
-//  Wire.endTransmission();     
-
+void loop(){
+Kommunikation->sendXY(42,47);// example position
+Kommunikation->sendXY(5,64);// example position
+  delay(500);
+  
+  Kommunikation->receiveXY();//get position from other vehicle. You can see the position as serial print...
+  Serial.println(Kommunikation->getX());// or use this member function to get position
+  delay(100);
 }
+
